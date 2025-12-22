b69c5a8667a0f0e6d3cfdf81765922d1efc4c2f1
Jack Chronicle <18507378+Jack-Chronicle@users.noreply.github.com>
2025-12-22 01:50:29 -0700

    Update flake.nix to use nixos-25.11 version

--- flake.nix
+++ flake.nix
@@ -6,8 +6,11 @@
   description = "Home Manager module: npm, pipx, eget, go package installer";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
