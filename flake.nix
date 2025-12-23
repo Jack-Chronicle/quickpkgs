7ff99429f3827d911c7973da699428eb167dd088
Jack Chronicle <nyx.lantern@pm.me>
2025-12-22 19:51:52 -0700

    commit again

--- flake.nix
+++ flake.nix
@@ -207,7 +207,7 @@
               for pkg in ${joinQuoted config.cargo.packages}; do
                 if ! cargo install --list | grep -q "$pkg"; then
                   echo "Installing cargo package $pkg..."
-                  cargo install --path $CARGO_HOME $pkg
+                  cargo install --root $CARGO_HOME $pkg
                 else
                   echo "$pkg already installed, skipping..."
                 fi
